57d53ca06287122afaafc397f42fc6a3244e7447
Mahonri Martinez <cripto3000z@gmail.com>
2022-05-04 16:31:32 -0500

    Mi primer commit

---
+++ Menu/Menutipohabitacion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Tipo habitacion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+<div class="container">
+</div>
+</nav>
+<body>
+    <form action="" method="post">
+        <h1>Menu Tipo habitacion</h1>
+        <a href="../Tipohabitacion.php">Agregar tipo habitacion</a>
+        <br>
+        <a href="../BuscarTipoHabitacion.php">Buscar tipo habitacion</a>
+        <br>
+        <a href="../MostrarTipoHabitacion.php">Mostrar Tipo de habitaciones</a>
+        <br>
+        <a href="../login.php">Regresar Menu</a>
+    </form>
+</body>
+</html>
